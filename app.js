73df6f48a7c0dd204335b638e387e63e4a6a169a
abdalazzezzalsalahat <abdalazzezzalsalahat@icloud.com>
2021-02-12 03:57:23 +0200

    solved thee array multiplication function

--- app.js
+++ app.js
@@ -95,7 +95,8 @@ testSumArray(testArray);
 
 /////////////////////////////////////
 /* Problem 5
-Write a function called multiplyArray() that takes an array of numbers as its argument and returns an array whose first element is the product of those numbers, and the second element is a string that EXACTLY follows this example and uses the values that were input into the function:
+Write a function called multiplyArray() that takes an array of numbers as its argument and returns an array whose first element is the product of those numbers,
+ and the second element is a string that EXACTLY follows this example and uses the values that were input into the function:
 
 "The numbers 2,3,4 have a product of 24."
 
@@ -106,10 +107,15 @@ Test this function by hand in the console to get it working, and when you think 
 // Write your code here
 function multiplyArray(multArr) {
   //eslint-disable-line
+  let arrMulti = 1;
+  for (let c = 0; c < testArray.length; c++){
+    arrMulti = arrMulti * testArray[c];
+  }
+  return [arrMulti, 'The numbers ' + testArray + ' have a product of ' + arrMulti + '.'];
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyArray(testArray);
+testMultiplyArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop.
 
